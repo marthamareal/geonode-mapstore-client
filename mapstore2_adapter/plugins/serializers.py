13d9dc47acaf948f5d32b4b46825007091c080e1
marthamareal <marthamareal@gmail.com>
2021-09-24 14:13:14 +0300

    [Fixes #508] Exclude vector layers from the preview legend
    Co-authored-by: mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>

--- mapstore2_adapter/plugins/serializers.py
+++ mapstore2_adapter/plugins/serializers.py
@@ -11,6 +11,7 @@
 
 from __future__ import absolute_import
 from uuid import uuid4
+from copy import deepcopy
 
 from rest_framework.exceptions import APIException
 import json
@@ -60,10 +61,10 @@ class GeoNodeSerializer(object):
         _map_name = serializer.validated_data['title'] or map_obj.title
         _map_title = serializer.validated_data['title'] or map_obj.title
         _map_abstract = serializer.validated_data.get('abstract', '') or '' if not hasattr(map_obj, 'abstract') else map_obj.abstract
-       
+        _data_blob = serializer.validated_data.get('blob')
+
         if data:
             try:
-                data_blob = data.copy()
                 _map_conf = dict(data)
                 _map_conf["about"] = {
                     "name": _map_name,
@@ -198,7 +199,7 @@ class GeoNodeSerializer(object):
                     if not map_obj.uuid:
                         map_obj.uuid=str(uuid4())
 
-                    map_obj.blob = data_blob
+                    map_obj.blob = _data_blob
                     # Update GeoNode Map
                     _map_conf['map'] = _map_obj
 
@@ -208,11 +209,15 @@ class GeoNodeSerializer(object):
                     serializer.instance = map_obj
                     serializer.save()
 
+                    # filter annotations
+                    new_conf = deepcopy(_map_conf)
+                    new_conf['map']['layers'] = [x for x in new_conf['map']['layers'] if x['type'] != 'vector']
+
                     serializer.instance.update_from_viewer(
-                        _map_conf,
-                        context={'config': _map_conf})
+                        new_conf,
+                        context={'config': new_conf})
                     return serializer
-                    
+
             except Exception as e:
                 tb = traceback.format_exc()
                 logger.error(tb)
